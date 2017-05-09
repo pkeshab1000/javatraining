cffd616f9caf35142517c24f2e8fcfb46134478c
pkeshab1000 <pkeshab1000@gmail.com>
2017-05-09 22:46:20 +0545

    first commit

---
+++ divisible.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter any number: \n");
+    scanf("%d",&n);
+    if(n%3==0){
+        printf("you entered number is divisible by 3 \n");
+    }
+    else printf("you entered number is not divisible by 3 \n");
+
+}
+
